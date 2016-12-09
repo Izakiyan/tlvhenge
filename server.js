fad54da37202803aa23696a6b689134b84bfcce0
orielbachar <oriel.bachar@gmail.com>
2016-12-09 11:56:37 +0200

    ready

--- server.js
+++ server.js
@@ -60,6 +60,11 @@ app.get('/streets', function (req, res) {
 
 app.listen(8000);
 
+server.listen(process.env.PORT || 3000, function() {
+   console.log('Example app listening on port 3000!')
+})
+
+
 
 
 
